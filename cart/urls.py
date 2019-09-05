e6060db68efd3e77ffca18c1dab9c6076d609d18
Benjamin Senyonyi <bejose2@yahoo.com>
2019-09-05 12:59:54 +0300

    relative import (top level) bug

--- cart/urls.py
+++ cart/urls.py
@@ -3,11 +3,16 @@ Created on 3 Sep 2019
 
 @author: benjaminsenyonyi
 '''
+# import sys
+# sys.path.append("..")
+
 from django.urls import path
 from django.conf.urls import url
-# from . import views #here
-# from views import cart_add, cart_detail, cart_remove
-# from .views import *
+from . import views #here
+# from .views import cart_add, cart_detail, cart_remove
+
+
+
 
 
 app_name = 'cart'
@@ -18,9 +23,18 @@ urlpatterns = [
 #     url('remove/<int:product_id>/', views.cart_remove, name = 'cart_remove'),
     #     url(r'^admin/', admin.site.urls),
 
-#     path('', views.cart_detail, name = 'cart_detail'),
-#     path('add/<int:product_id>/', views.cart_add, name = 'cart_add'),
-#     path('remove/<int:product_id>/', views.cart_remove, name = 'cart_remove'),
+    path('', views.cart_detail, name = 'cart_detail'),
+    path('add/<int:product_id>/', views.cart_add, name = 'cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name = 'cart_remove'),
+
+#     path('', views.product_list, name  = 'product_list'),
+#     path('<slug:category_slug>/', views.product_list, name= 'product_list_by_category'),
+#     path('<int:id>/<slug:slug>/', views.product_detail, name = 'product_detail'),
+    
+#     path('', cart_detail, name = 'cart_detail'),
+#     path('add/<int:product_id>/', cart_add, name = 'cart_add'),
+#     path('remove/<int:product_id>/', cart_remove, name = 'cart_remove'),
+#     
     ]
 
 
--- cart/views.py
+++ cart/views.py
@@ -36,3 +36,23 @@ def cart_detail(request):
     '''
     cart =Cart(request)
     return render(request, 'cart/detail.html', {'cart' : cart})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product':product, 'cart_product_form':cart_product_form})
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
